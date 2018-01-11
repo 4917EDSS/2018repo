bcc60b1b18a8c43b8299dcc53a74ea28cc69ba75
4917 Programmer Driver Station <4917programmer@gmail.com>
2018-01-11 17:19:44 -0500

    Fixed some spacing

--- 2018CompetitionBot/src/Subsystems/DrivetrainSub.cpp
+++ 2018CompetitionBot/src/Subsystems/DrivetrainSub.cpp
@@ -4,22 +4,21 @@
 
 
 DrivetrainSub::DrivetrainSub() : Subsystem("DrivetrainSub") {
-  leftMotor1.reset(new TalonSRX(LEFT1_DRIVE_MOTOR_CANID));
-  leftMotor2.reset(new TalonSRX(LEFT2_DRIVE_MOTOR_CANID));
-  leftMotor3.reset(new TalonSRX(LEFT3_DRIVE_MOTOR_CANID));
-  rightMotor1.reset(new TalonSRX(RIGHT1_DRIVE_MOTOR_CANID));
-  rightMotor2.reset(new TalonSRX(RIGHT2_DRIVE_MOTOR_CANID));
-  rightMotor3.reset(new TalonSRX(RIGHT3_DRIVE_MOTOR_CANID));
+	leftMotor1.reset(new TalonSRX(LEFT1_DRIVE_MOTOR_CANID));
+	leftMotor2.reset(new TalonSRX(LEFT2_DRIVE_MOTOR_CANID));
+	leftMotor3.reset(new TalonSRX(LEFT3_DRIVE_MOTOR_CANID));
+	rightMotor1.reset(new TalonSRX(RIGHT1_DRIVE_MOTOR_CANID));
+	rightMotor2.reset(new TalonSRX(RIGHT2_DRIVE_MOTOR_CANID));
+	rightMotor3.reset(new TalonSRX(RIGHT3_DRIVE_MOTOR_CANID));
 }
 
 void DrivetrainSub::drive(double lSpeed, double rSpeed) {
-
- leftMotor1->Set(ControlMode::PercentOutput, -lSpeed);
- leftMotor2->Set(ControlMode::PercentOutput, -lSpeed);
- leftMotor3->Set(ControlMode::PercentOutput, -lSpeed);
- rightMotor1->Set(ControlMode::PercentOutput, rSpeed);
- rightMotor2->Set(ControlMode::PercentOutput, rSpeed);
- rightMotor3->Set(ControlMode::PercentOutput, rSpeed);
+	leftMotor1->Set(ControlMode::PercentOutput, -lSpeed);
+	leftMotor2->Set(ControlMode::PercentOutput, -lSpeed);
+	leftMotor3->Set(ControlMode::PercentOutput, -lSpeed);
+	rightMotor1->Set(ControlMode::PercentOutput, rSpeed);
+	rightMotor2->Set(ControlMode::PercentOutput, rSpeed);
+	rightMotor3->Set(ControlMode::PercentOutput, rSpeed);
 }
 
 void DrivetrainSub::InitDefaultCommand() {
