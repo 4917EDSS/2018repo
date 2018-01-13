b0ca8d25ece0fd18ec6633c9fccadb1464a9020b
Eric Gaudet <ericjpgaudet@yahoo.ca>
2018-01-13 13:03:02 -0500

    Renamed RunIntakeCmd to IntakeBoxCmd

--- 2018CompetitionBot/src/Commands/IntakeBoxCmd.cpp
+++ 2018CompetitionBot/src/Commands/IntakeBoxCmd.cpp
@@ -1,34 +1,34 @@
-#include "RunIntakeCmd.h"
+#include <Commands/IntakeBoxCmd.h>
 #include "OI.h"
 
-RunIntakeCmd::RunIntakeCmd() {
+IntakeBoxCmd::IntakeBoxCmd() {
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(Robot::chassis.get());
 	Requires(intakeSub.get());
 }
 
 // Called just before this Command runs the first time
-void RunIntakeCmd::Initialize() {
+void IntakeBoxCmd::Initialize() {
 	intakeSub->intake(1.0);
 }
 
 // Called repeatedly when this Command is scheduled to run
-void RunIntakeCmd::Execute() {
+void IntakeBoxCmd::Execute() {
 
 }
 
 // Make this return true when this Command no longer needs to run execute()
-bool RunIntakeCmd::IsFinished() {
+bool IntakeBoxCmd::IsFinished() {
 	return intakeSub->IsLimitHit();
 }
 
 // Called once after isFinished returns true
-void RunIntakeCmd::End() {
+void IntakeBoxCmd::End() {
 	intakeSub->intake(0.0);
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
-void RunIntakeCmd::Interrupted() {
+void IntakeBoxCmd::Interrupted() {
 	End();
 }
--- 2018CompetitionBot/src/OI.cpp
+++ 2018CompetitionBot/src/OI.cpp
@@ -1,14 +1,14 @@
+#include <Commands/IntakeBoxCmd.h>
 #include "OI.h"
 
 #include <WPILib.h>
-#include "Commands/RunIntakeCmd.h"
 #include "Commands/ReverseIntakeCmd.h"
 
 OI::OI() {
 	// Process operator interface input here.
 	driverController.reset(new frc::Joystick(DRIVER_CONTROLLER_PORT));
 	intakeBtn.reset(new frc::JoystickButton(driverController.get(), INTAKE_BTN));
-	intakeBtn->WhenPressed(new RunIntakeCmd());
+	intakeBtn->WhenPressed(new IntakeBoxCmd());
 	reverseIntakeBtn.reset(new frc::JoystickButton(driverController.get(), REVERSE_INTAKE_BTN));
 	reverseIntakeBtn->WhileHeld(new ReverseIntakeCmd());
 }
