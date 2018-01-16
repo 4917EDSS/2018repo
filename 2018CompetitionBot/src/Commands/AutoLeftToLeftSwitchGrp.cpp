13bc0e1c31220ac394db9a07aaa977eefabad15c
EDSS Programmer <frc4917@gmail.com>
2018-01-16 17:38:40 -0500

    More random auto starts

--- 2018CompetitionBot/src/Commands/AutoLeftToLeftSwitchGrp.cpp
+++ 2018CompetitionBot/src/Commands/AutoLeftToLeftSwitchGrp.cpp
@@ -1,20 +1,10 @@
 #include "AutoLeftToLeftSwitchGrp.h"
-
+#include "Commands/DriveStraightCmd.cpp"
+#include "Commands/DriveTurnCmd.cpp"
 AutoLeftToLeftSwitchGrp::AutoLeftToLeftSwitchGrp() {
-	// Add Commands here:
-	// e.g. AddSequential(new Command1());
-	//      AddSequential(new Command2());
-	// these will run in order.
 
-	// To run multiple commands at the same time,
-	// use AddParallel()
-	// e.g. AddParallel(new Command1());
-	//      AddSequential(new Command2());
-	// Command1 and Command2 will run in parallel.
-
-	// A command group will require all of the subsystems that each member
-	// would require.
-	// e.g. if Command1 requires chassis, and Command2 requires arm,
-	// a CommandGroup containing them would require both the chassis and the
-	// arm.
+	AddSequential(new DriveStraightCmd(3798));
+	AddSequential(new DriveTurnCmd(90));
+	AddSequential(new DriveStraightCmd(202));
+	//Drop Box
 }
