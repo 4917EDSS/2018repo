99e82dee781448be44ac3410fb4780fb53fc0bac
4917 clunker 1 <4917programmer@gmail.com>
2018-01-27 12:04:02 -0500

    Rewrote the AutoScaleLeftToRight auto

--- 2018CompetitionBot/src/Commands/AutoScaleLeftToRightGrp.cpp
+++ 2018CompetitionBot/src/Commands/AutoScaleLeftToRightGrp.cpp
@@ -3,28 +3,11 @@
 #include <Commands/DriveTurnCmd.h>
 
 AutoScaleLeftToRightGrp::AutoScaleLeftToRightGrp() {
-	AddSequential(new DriveStraightCmd(3000,0.0));
-	AddSequential(new DriveTurnCmd(-90));
-	AddSequential(new DriveStraightCmd(3000,-90.0));
-	AddSequential(new DriveTurnCmd(-90));
-	AddSequential(new DriveStraightCmd(3000,-180.0));
-	AddSequential(new DriveTurnCmd(-90));
-	AddSequential(new DriveStraightCmd(3000,90.0));
-	AddSequential(new DriveTurnCmd(-90));
-	AddSequential(new DriveStraightCmd(3000,0.0));
-	AddSequential(new DriveTurnCmd(-90));
-	AddSequential(new DriveStraightCmd(3000,-90.0));
-	AddSequential(new DriveTurnCmd(-90));
-	AddSequential(new DriveTurnCmd(120));
-	AddSequential(new DriveTurnCmd(120));
-	AddSequential(new DriveTurnCmd(120));
-	AddSequential(new DriveStraightCmd(-1500,-180.0));
-	AddSequential(new DriveTurnCmd(-72.0));
-	AddSequential(new DriveStraightCmd(2000,102.0));
-	AddSequential(new DriveTurnCmd(-108));
-	AddSequential(new DriveStraightCmd(1000,-6.0));
-	AddSequential(new DriveTurnCmd(-90));
-	AddSequential(new DriveStraightCmd(1732,-96.0));
-	AddSequential(new DriveStraightCmd(550,-96.0));
-
+AddSequential(new DriveStraightCmd(6803,0.0));
+AddSequential(new DriveTurnCmd(90));
+AddSequential(new DriveStraightCmd(5799,90));
+AddSequential(new DriveTurnCmd(-90));
+AddSequential(new DriveStraightCmd(2395,0.0));
+AddSequential(new DriveTurnCmd(-90));
+AddSequential(new DriveStraightCmd(1075,-90));
 }
