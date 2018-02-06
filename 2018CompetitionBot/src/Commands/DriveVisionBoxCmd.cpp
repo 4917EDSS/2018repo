7c97072416e81fc8972d53917a579de3a0aef2c0
EDSS Programmer <frc4917@gmail.com>
2018-02-06 16:48:28 -0500

    Added Driving to a Box until it is picked up

---
+++ 2018CompetitionBot/src/Commands/DriveVisionBoxCmd.cpp
@@ -0,0 +1,41 @@
+#include "DriveVisionBoxCmd.h"
+
+DriveVisionBoxCmd::DriveVisionBoxCmd() {
+}
+
+// Called just before this Command runs the first time
+void DriveVisionBoxCmd::Initialize() {
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void DriveVisionBoxCmd::Execute() {
+	float Kp = 0.1;  // Proportional control constant
+
+	std::shared_ptr<NetworkTable> table = NetworkTable::GetTable("limelight");
+	float tx = table->GetNumber("tx", 0.0);
+	float steering_adjust = Kp*tx;
+	drivetrainSub->drive(0.5+steering_adjust, 0.5-steering_adjust);
+
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool DriveVisionBoxCmd::IsFinished() {
+	if (intakeSub->isBoxIn()==true){
+		return true;
+	}
+	return false;
+}
+
+// Called once after isFinished returns true
+void DriveVisionBoxCmd::End() {
+	drivetrainSub->drive(0,0);
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void DriveVisionBoxCmd::Interrupted() {
+	End();
+
+}
