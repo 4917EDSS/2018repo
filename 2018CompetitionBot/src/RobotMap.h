a8fb5d4758e03b58bcfa638b1afdc9e2e4f4e036
EDSS Programmer <frc4917@gmail.com>
2018-02-08 16:07:03 -0500

    Prints when the box is in intake

--- 2018CompetitionBot/src/RobotMap.h
+++ 2018CompetitionBot/src/RobotMap.h
@@ -1,7 +1,7 @@
 #ifndef ROBOTMAP_H
 #define ROBOTMAP_H
 
-/**
+/*
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
  * the wiring easier and significantly reduces the number of magic numbers
