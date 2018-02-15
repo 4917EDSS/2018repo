98c63699ad21c33c1762f948af9a073c66beca3e
4917 Sonny <4917programmer@gmail.com>
2018-02-15 17:38:49 -0500

    Added Setting the camera off and correct piple to DriveVisionBoxCmd

--- 2018CompetitionBot/src/Commands/DriveVisionBoxCmd.cpp
+++ 2018CompetitionBot/src/Commands/DriveVisionBoxCmd.cpp
@@ -1,22 +1,24 @@
 #include "DriveVisionBoxCmd.h"
+#include <networktables/NetworkTableInstance.h>
 
 DriveVisionBoxCmd::DriveVisionBoxCmd() {
 }
 
 // Called just before this Command runs the first time
 void DriveVisionBoxCmd::Initialize() {
-
+	nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetEntry("pipeline").SetDouble(1.0);
+	nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetEntry("ledMode").SetDouble(1.0);
 }
 
 // Called repeatedly when this Command is scheduled to run
 void DriveVisionBoxCmd::Execute() {
 	float Kp = 0.1;  // Proportional control constant
-
-	std::shared_ptr<NetworkTable> table = NetworkTable::GetTable("limelight");
+	//nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+	std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
 	float tx = table->GetNumber("tx", 0.0);
 	float steering_adjust = Kp*tx;
 	drivetrainSub->drive(0.5+steering_adjust, 0.5-steering_adjust);
-
+	//NetworkTable::GetTable("limelight");
 }
 
 // Make this return true when this Command no longer needs to run execute()
