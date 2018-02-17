3d87706fa0ac6abb8428b05c0c494b804ffc247d
EDSS Programmer <frc4917@gmail.com>
2018-02-17 11:31:13 -0500

    Made changes to auto scale right

--- 2018CompetitionBot/src/Commands/AutoScaleRightGrp.cpp
+++ 2018CompetitionBot/src/Commands/AutoScaleRightGrp.cpp
@@ -1,20 +1,25 @@
 #include "Commands/DriveStraightCmd.h"
 #include "Commands/DriveTurnCmd.h"
 #include "AutoScaleRightGrp.h"
+#include "MoveElevatorToHeightCmd.h"
+#include "ReverseIntakeCmd.h"
 
 AutoScaleRightGrp::AutoScaleRightGrp() {
 	// Add Commands here:
 	// e.g. AddSequential(new Command1());
 	//      AddSequential(new Command2());
 	// these will run in order.
-
+	AddParallel(new MoveElevatorToHeightCmd(20));
 
 	AddSequential(new DriveStraightCmd(7500,0.0));
+
+	AddParallel(new MoveElevatorToHeightCmd(500));
 
 	AddSequential(new DriveTurnCmd(-90));
 
 	AddSequential(new DriveStraightCmd(200,-90.0));
 
+	AddSequential(new ReverseIntakeCmd(1));
 
 	// A command group will require all of the subsystems that each member
 	// would require.
