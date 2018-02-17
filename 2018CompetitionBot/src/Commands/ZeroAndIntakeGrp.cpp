d6758b3500bff09a83e439cf88c87ca9fe693887
4917 Vincent <4917programmer@gmail.com>
2018-02-17 15:15:06 -0500

    Added an intake at height

--- 2018CompetitionBot/src/Commands/ZeroAndIntakeGrp.cpp
+++ 2018CompetitionBot/src/Commands/ZeroAndIntakeGrp.cpp
@@ -1,9 +1,11 @@
 #include "ZeroAndIntakeGrp.h"
 #include "ZeroElevatorCmd.h"
 #include "IntakeUntilLimitCmd.h"
+#include "MoveElevatorToHeightCmd.h"
 
 ZeroAndIntakeGrp::ZeroAndIntakeGrp() {
 	AddSequential(new ZeroElevatorCmd());
 	AddSequential(new IntakeUntilLimitCmd());
+	AddSequential(new MoveElevatorToHeightCmd(60));
 	// TODO: add raise height
 }
