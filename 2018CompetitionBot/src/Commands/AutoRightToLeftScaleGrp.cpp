1cb30752546327956ebd0866ad822d3ccc6a8a9f
4917 Chappie <4917programmer@gmail.com>
2018-03-10 16:12:47 -0500

    Auto RtoLScale tweaks

--- 2018CompetitionBot/src/Commands/AutoRightToLeftScaleGrp.cpp
+++ 2018CompetitionBot/src/Commands/AutoRightToLeftScaleGrp.cpp
@@ -22,11 +22,11 @@ AutoRightToLeftScaleGrp::AutoRightToLeftScaleGrp() {
 
 	AddParallel(new DelayedElevatorToHeightGrp(5,ElevatorSub::SCALE_BOX_HIGH_HEIGHT));
 
-	AddSequential(new SilkyMotionCmd(std::vector<double> {4380, 2000, 4000, 2200}, std::vector<double> {0, -90, 0, 150}));
+	AddSequential(new SilkyMotionCmd(std::vector<double> {4380, 2000, 4100, 2200}, std::vector<double> {0, -90, 0, 150}));
 
 	AddSequential(new ReverseIntakeCmd(0.5));
 
-	AddSequential(new DriveStraightCmd(-500,60));
+	AddSequential(new DriveStraightCmd(-700,60));
 
 	AddSequential(new ZeroElevatorCmd());
 
