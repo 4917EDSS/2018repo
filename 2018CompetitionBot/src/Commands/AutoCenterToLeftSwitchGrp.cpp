6e9cd942740089ada13ed2d9567c0c0aa07dabf8
4917 Sonny <4917programmer@gmail.com>
2018-03-13 10:42:44 -0400

    Silky-ising autos

--- 2018CompetitionBot/src/Commands/AutoCenterToLeftSwitchGrp.cpp
+++ 2018CompetitionBot/src/Commands/AutoCenterToLeftSwitchGrp.cpp
@@ -7,6 +7,7 @@
 #include "Commands/ZeroElevatorCmd.h"
 #include "Commands/IntakeUntilLimitCmd.h"
 #include "Subsystems/IntakeSub.h"
+#include "Commands/SilkyMotionCmd.h"
 
 AutoCenterToLeftSwitchGrp::AutoCenterToLeftSwitchGrp() {
 	// Add Commands here:
@@ -21,32 +22,31 @@ AutoCenterToLeftSwitchGrp::AutoCenterToLeftSwitchGrp() {
 
 	AddParallel(new MoveElevatorToHeightCmd(ElevatorSub::SWITCH_BOX_HEIGHT));
 
-	heading = -25;
-	AddSequential(new DriveTurnCmd(heading));
+	heading = -30;
+	AddSequential(new SilkyMotionCmd(std::vector<double> {1500, 1250}, std::vector<double> {heading, 0}));
 
-	AddSequential(new DriveStraightCmd(2750,heading));
+	//AddSequential(new DriveStraightCmd(2750,heading));
 
 	AddSequential(new ReverseIntakeCmd(0.5));
 
 	AddParallel(new ZeroElevatorCmd());
 
-	AddSequential(new DriveStraightCmd(-1250,heading));
+	AddSequential(new SilkyMotionCmd(std::vector<double> {-850, -400}, std::vector<double> {0, 30}));
+	//AddSequential(new DriveStraightCmd(-1250,heading));
 
-	heading = 25;
-	AddSequential(new DriveTurnCmd(heading));
+	//AddSequential(new DriveTurnCmd(heading));   //Towards second box
 
 	AddParallel(new IntakeUntilLimitCmd());
 
 	AddSequential(new DriveStraightCmd(400,heading));
 
-	AddSequential(new DriveStraightCmd(-400,heading));
+	AddSequential(new SilkyMotionCmd(std::vector<double> {-300, -300}, std::vector<double> {45, -45}));
+	//AddSequential(new DriveStraightCmd(-400,heading));
 
 	AddParallel(new MoveElevatorToHeightCmd(ElevatorSub::SWITCH_BOX_HEIGHT));
 
-	heading = -25;
-	AddSequential(new DriveTurnCmd(heading));
-
-	AddSequential(new DriveStraightCmd(1400,heading));
+	AddSequential(new DriveStraightCmd(1000,0));
+	//AddSequential(new DriveStraightCmd(1400,heading));
 
 	AddSequential(new ReverseIntakeCmd(0.5));
 	// To run multiple commands at the same time,
