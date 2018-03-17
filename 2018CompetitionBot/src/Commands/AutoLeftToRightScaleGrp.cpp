dfb829a87c43317b90dba9f456dd2838c9de1747
4917 Sonny <4917programmer@gmail.com>
2018-03-17 14:52:24 -0400

    left to right scale

--- 2018CompetitionBot/src/Commands/AutoLeftToRightScaleGrp.cpp
+++ 2018CompetitionBot/src/Commands/AutoLeftToRightScaleGrp.cpp
@@ -12,27 +12,28 @@
 
 AutoLeftToRightScaleGrp::AutoLeftToRightScaleGrp() {
 
+
 	AddParallel (new FoldArmsDownCmd());
 
-	AddParallel(new DelayedElevatorToHeightGrp(0,4.75,ElevatorSub::SCALE_BOX_HIGH_HEIGHT));
+	AddParallel(new DelayedElevatorToHeightGrp(ElevatorSub::CARRY_HEIGHT,4.75,ElevatorSub::SCALE_BOX_HIGH_HEIGHT));
 
-	AddSequential(new SilkyMotionCmd(std::vector<double> {4250, 2300, 3340, 1300}, std::vector<double> {0, 90, 0, -120}));
+	AddSequential(new SilkyMotionCmd(std::vector<double> {4210, 2300, 3340, 1300}, std::vector<double> {0, -90, 0, 120}));
 
 	AddSequential(new ReverseIntakeCmd(0.4));
 
 	AddParallel(new IntakeUntilLimitCmd());
 
-	AddParallel(new DelayedElevatorToHeightGrp(0,0.5,0));
+	AddParallel(new DelayedElevatorToHeightGrp(0.5,0.0));
 
-	AddSequential(new SilkyMotionCmd(std::vector<double>{-1000,1450}, std::vector<double>{-80, -45}));
+	AddSequential(new SilkyMotionCmd(std::vector<double>{-1000,1600}, std::vector<double>{80, 45}));
 
-	AddParallel(new DelayedElevatorToHeightGrp(0,0.5,ElevatorSub::SCALE_BOX_HIGH_HEIGHT));
+	AddParallel(new MoveElevatorToHeightCmd(ElevatorSub::SCALE_BOX_HIGH_HEIGHT));
 
-	AddSequential(new SilkyMotionCmd(std::vector<double>{-1450, 1000}, std::vector<double>{45, 80}));
+	AddSequential(new SilkyMotionCmd(std::vector<double>{-1600, 1000}, std::vector<double>{-45, -70}));
 
-	AddSequential(new ReverseIntakeCmd(0.4));
+	AddSequential(new ReverseIntakeCmd(0.1));
 
-	AddParallel(new DelayedElevatorToHeightGrp(0,0.5,0));
+	AddParallel(new DelayedElevatorToHeightGrp(0.5,0));
 
 	AddSequential(new SilkyMotionCmd(std::vector<double>{-700}, std::vector<double>{0}));
-}
+
