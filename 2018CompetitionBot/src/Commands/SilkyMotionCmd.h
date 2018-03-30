f0cea60030590cb608967da4a11b5283dfe36866
4917 Jarvis <4917programmer@gmail.com>
2018-03-30 08:41:09 -0400

    Changed Max velocity in silky motion

--- 2018CompetitionBot/src/Commands/SilkyMotionCmd.h
+++ 2018CompetitionBot/src/Commands/SilkyMotionCmd.h
@@ -6,13 +6,13 @@
 
 const double MAX_LIN_ACCEL=2200;
 const double MAX_LIN_DECEL=3000.0;
-const double MAX_LIN_VEL=2650;
+const double MAX_LIN_VEL=2400;
 const double MAX_ANG_ACCEL=800.0;
 
 const double P_DIS=0.0075;
 const double D_DIS=0.000002;
-const double A_DIS=0.3*(0.8/MAX_LIN_VEL);
-const double V_DIS=1*(0.8/MAX_LIN_VEL);
+const double A_DIS=0.3*(0.8/2650);
+const double V_DIS=1*(0.8/2650);
 const double P_ANG=0.039;
 //When we drove at these speeds we were at 0.3 power we got 50 degrees per second
 const double V_ANG=0.8*(0.3/50);
