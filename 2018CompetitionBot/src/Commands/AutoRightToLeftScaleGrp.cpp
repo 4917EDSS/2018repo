3dff31994b23ec34dd8c0b425aaeb04cabcae430
4917 Jarvis <4917programmer@gmail.com>
2018-04-13 15:40:31 -0400

    Tuning Right to left Scale

--- 2018CompetitionBot/src/Commands/AutoRightToLeftScaleGrp.cpp
+++ 2018CompetitionBot/src/Commands/AutoRightToLeftScaleGrp.cpp
@@ -16,10 +16,10 @@ AutoRightToLeftScaleGrp::AutoRightToLeftScaleGrp() {
 
 	AddParallel(new IntakeUntilLimitCmd());
 	AddParallel(new DelayedElevatorToHeightGrp(0.5,0.0));
-	AddSequential(new SilkyMotionCmd(std::vector<double>{-800,2700}, std::vector<double>{35, 61}));
+	AddSequential(new SilkyMotionCmd(std::vector<double>{-800,2700}, std::vector<double>{35, 75}));
 
-	AddParallel(new MoveElevatorToHeightCmd(ElevatorSub::SCALE_BOX_HIGH_HEIGHT));
-	AddSequential(new SilkyMotionCmd(std::vector<double>{-2700, 800}, std::vector<double>{-61, -35}));
+	AddParallel(new MoveElevatorToHeightCmd(ElevatorSub::AUTO_FIRST_BOX_HEIGHT));
+	AddSequential(new SilkyMotionCmd(std::vector<double>{-2700, 800}, std::vector<double>{-75, -35}));
 
 	AddSequential(new ReverseIntakeCmd(0.4));
 
