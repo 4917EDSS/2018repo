eaf801d9c7adadfa5f8118812da56ba15dd73729
4917 Jarvis <4917programmer@gmail.com>
2018-04-21 10:03:13 -0400

    victor to talon, Dont forget to switch back!!

--- 2018CompetitionBot/src/Subsystems/IntakeSub.cpp
+++ 2018CompetitionBot/src/Subsystems/IntakeSub.cpp
@@ -13,7 +13,9 @@ IntakeSub::IntakeSub() : Subsystem("IntakeSub") {
 	intakeMotorRight.reset(new WPI_TalonSRX(INTAKE_MOTOR_RIGHT_CANID));
 	intakeMotorRight->SetName("Intake", "Right Motor");
 
-	armFolderMotor.reset(new WPI_VictorSPX(ARM_FOLDER_CANID));
+
+	//Make this a victor!!!!!!!!!!!
+	armFolderMotor.reset(new WPI_TalonSRX(ARM_FOLDER_CANID));
 	armFolderMotor->SetName("Intake", "Arm Folder Motor");
 
 	intakeCloseLimit.reset(new DigitalInput(INTAKE_LIMIT_CLOSE_DIO));
